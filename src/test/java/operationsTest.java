88694a6653740b64861d4142e984c67a9f06583d
josephShokry <josephshokry9@gmail.com>
2022-10-01 15:22:36 +0200

    solving the running test

--- src/test/java/operationsTest.java
+++ src/test/java/operationsTest.java
@@ -14,14 +14,15 @@ class operationsTest {
     @Test
     void creatTest() throws JSONException, IOException {
         String schemaPath = "C:\\Users\\lenovo\\Desktop\\check_in_system\\DB\\SimpleFSDB\\tests\\testcases_schemas\\schema.txt";
-        when(mockedExecute.execute(Paths.get("source","main.py") + " -cmd create -sch " + schemaPath)).thenReturn(
+        String command = Paths.get("source","main.py") + " -cmd create -sch " + schemaPath;
+        when(mockedExecute.execute(command)).thenReturn(
                 new JSONObject("{'result': 'None', 'message': 'the command has been executed successfully!', 'status': 'Success'}"));
-        try{
-            new Driver().createDatabase("C:\\Users\\lenovo\\Desktop\\check_in_system\\DB\\SimpleFSDB\\tests\\testcases_schemas\\schema.txt");
-        }
-        catch (Exception e){
-            fail("the command raised error");
-        }
+//        try{
+            new Driver().createDatabase(schemaPath);
+//        }
+//        catch (Exception e){
+//            fail("the command raised error");
+//        }
     }
     @Test
     void notGivingSchema() throws JSONException, IOException {//not giving the schema or the schema file is not proper
