a11106c1759365bb6cc6b16546ff9d77e2cac93e
josephShokry <josephshokry9@gmail.com>
2022-10-03 09:22:25 +0200

    fix comments

--- src/main/java/Executer.java
+++ src/main/java/Executer.java
@@ -6,29 +6,33 @@ import java.nio.file.Paths;
 import java.util.Properties;
 
 public final class Executer {
-    private String databasePath;
-    private String pythonCaller;
+    private final String databasePath;
+    private final String pythonCaller;
     private static Executer executerObject = null;
+    
     private Executer() throws IOException {
         Path filePath = Paths.get(Paths.get(System.getProperty("user.dir")).toString(), "src", "main", "java", "configurations.properties");
-        loadConfg(filePath.toString());
+        Properties confg = loadConfg(filePath.toString());
+        this.databasePath = confg.getProperty("databasePath");
+        this.pythonCaller = confg.getProperty("pythonCaller");
+
     }
+
     public static Executer getInstance() throws IOException {
         if(executerObject == null){
             executerObject = new Executer();
         }
         return executerObject;
     }
-    private void loadConfg(String confgFilePath) throws IOException {
+    private Properties loadConfg(String confgFilePath) throws IOException {
         final FileReader confgFile = new FileReader(confgFilePath);
         final Properties confg = new Properties();
         confg.load(confgFile);
-        this.databasePath = confg.getProperty("databasePath");
-        this.pythonCaller = confg.getProperty("pythonCaller");
         confgFile.close();
+        return confg;
     }
     JSONObject execute(String command) throws IOException, JSONException {
-        final File dir = new File(databasePath);
+        final File dir = new File(this.databasePath);
         final Process process = Runtime.getRuntime().exec(pythonCaller + " " + command,null,dir);
         final BufferedReader outputMessage = new BufferedReader(new InputStreamReader(process.getInputStream()));
         return new JSONObject(outputMessage.readLine());
